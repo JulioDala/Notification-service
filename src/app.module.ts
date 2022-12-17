6d54a62c2f57f3c120754c9658d3108857b31bb9
Júlio A. Dala <you@example.com>
2022-12-17 22:30:00 +0100

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [PrismaService],
+})
+// eslint-disable-next-line prettier/prettier
+export class AppModule { }
